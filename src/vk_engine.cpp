1ed03c7a97d101370205678f87c235838802cc1d
mhubczenko <mhubczenko@tantalus.com.au>
2024-02-29 22:57:08 +1030

    Added basic init code and command pools

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -7,13 +7,18 @@
 #include <vk_initializers.h>
 #include <vk_types.h>
 
+//bootstrap library
+#include "VkBootstrap.h"
+
 #include <chrono>
 #include <thread>
 
 VulkanEngine* loadedEngine = nullptr;
-
 VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
-void VulkanEngine::init()
+
+constexpr bool bUseValidationLayers = true;
+
+void VulkanEngine::Init()
 {
     // only one engine initialization is allowed with the application.
     assert(loadedEngine == nullptr);
@@ -24,35 +29,177 @@ void VulkanEngine::init()
 
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
 
-    _window = SDL_CreateWindow(
+    mWindow = SDL_CreateWindow(
         "Vulkan Engine",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        _windowExtent.width,
-        _windowExtent.height,
+        mWindowExtent.width,
+        mWindowExtent.height,
         window_flags);
 
+	InitVulkan();
+	InitSwapchain();
+	InitCommands();
+	InitSyncStructures();
+
     // everything went fine
-    _isInitialized = true;
+    mIsInitialized = true;
 }
 
-void VulkanEngine::cleanup()
+void VulkanEngine::InitVulkan()
 {
-    if (_isInitialized) {
+	vkb::InstanceBuilder builder;
 
-        SDL_DestroyWindow(_window);
+	//make the vulkan instance, with basic debug features
+	auto inst_ret = builder.set_app_name("Example Vulkan Application")
+		.request_validation_layers(bUseValidationLayers)
+		.use_default_debug_messenger()
+		.require_api_version(1, 3, 0)
+		.build();
+
+	vkb::Instance vkb_inst = inst_ret.value();
+
+	// grab the instance
+	mInstance = vkb_inst.instance;
+	mDebugMessenger = vkb_inst.debug_messenger;
+
+	SDL_Vulkan_CreateSurface(mWindow, mInstance, &mSurface);
+
+	// vulkan 1.3 features
+	VkPhysicalDeviceVulkan13Features features13{};
+	features13.dynamicRendering = true;
+	features13.synchronization2 = true;
+
+	// vulkan 1.2 features
+	VkPhysicalDeviceVulkan12Features features12{};
+	features12.bufferDeviceAddress = true;
+	features12.descriptorIndexing = true;
+
+	// use vkbootstrap to select a gpu.
+	// We want a gpu that can write to the SDL surface and supports vulkan 1.3 with the correct features
+	vkb::PhysicalDeviceSelector selector{ vkb_inst };
+	vkb::PhysicalDevice physicalDevice = selector
+		.set_minimum_version(1, 3)
+		.set_required_features_13(features13)
+		.set_required_features_12(features12)
+		.set_surface(mSurface)
+		.select()
+		.value();
+
+	// Create the final vulkan device
+	vkb::DeviceBuilder deviceBuilder{ physicalDevice};
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	// Get the VkDevice handle used in the rest of the vulkan application
+	mDevice = vkbDevice.device;
+	mChosenGPU = physicalDevice.physical_device;
+
+	// use vkbootstrap to get a Graphics queue
+	mGraphicsQueue = vkbDevice.get_queue(vkb::QueueType::graphics).value();
+	mGraphicsQueueFamily = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+}
+
+void VulkanEngine::InitSwapchain()
+{
+	CreateSwapchain(mWindowExtent.width, mWindowExtent.height);
+}
+
+void VulkanEngine::CreateSwapchain(uint32_t width, uint32_t height)
+{
+	vkb::SwapchainBuilder swapchainBuilder{ mChosenGPU, mDevice, mSurface};
+	mSwapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+
+	vkb::Swapchain vkbSwapchain = swapchainBuilder
+		// use default_format_selection()
+		.set_desired_format(VkSurfaceFormatKHR{ .format = mSwapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
+		// use vysnc present mode
+		// The most important detail here is the present mode, which we have set to VK_PRESENT_MODE_FIFO_KHR. 
+		// This way we are doing a hard VSync, which will limit the FPS of the entire engine to the speed of the monitor.
+		.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+		.set_desired_extent(width, height)
+		.add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+		.build()
+		.value();
+
+	mSwapchainExtent = vkbSwapchain.extent;
+	// store swapchain and its related iamges
+	mSwapchain = vkbSwapchain.swapchain;
+	mSwapchainImages = vkbSwapchain.get_images().value();
+	mSwapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::DestroySwapchain()
+{
+	// We first delete the swapchain object, which will delete the images it holds internally. 
+	//  We then have to destroy the ImageViews for those images.
+
+	vkDestroySwapchainKHR(mDevice, mSwapchain, nullptr);
+
+	// destroy swapchain resources
+	for (int i = 0; i < mSwapchainImageViews.size(); i++) {
+
+		vkDestroyImageView(mDevice, mSwapchainImageViews[i], nullptr);
+	}
+}
+
+void VulkanEngine::InitCommands()
+{
+	//create a command pool for commands submitted to the graphics queue.
+	//we also want the pool to allow for resetting of individual command buffers
+	VkCommandPoolCreateInfo commandPoolInfo = vkinit::command_pool_create_info(mGraphicsQueueFamily, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+
+	for (int i = 0; i < FRAME_OVERLAP; i++) {
+
+		VK_CHECK(vkCreateCommandPool(mDevice, &commandPoolInfo, nullptr, &mFrames[i].mCommandPool));
+
+		// allocate the default command buffer that we will use for rendering
+		VkCommandBufferAllocateInfo cmdAllocInfo = vkinit::command_buffer_allocate_info(mFrames[i].mCommandPool, 1);
+
+		VK_CHECK(vkAllocateCommandBuffers(mDevice, &cmdAllocInfo, &mFrames[i].mMainCommandBuffer));
+	}
+}
+
+void VulkanEngine::InitSyncStructures()
+{
+
+}
+
+void VulkanEngine::Cleanup()
+{
+    if (mIsInitialized) {
+
+		//make sure the gpu has stopped doing its things
+		vkDeviceWaitIdle(mDevice);
+
+		for (int i = 0; i < FRAME_OVERLAP; i++) {
+			vkDestroyCommandPool(mDevice, mFrames[i].mCommandPool, nullptr);
+			// This will also destroy all CommandBuffers linked to the CommandPool
+		}
+
+
+		DestroySwapchain();
+
+		vkDestroySurfaceKHR(mInstance, mSurface, nullptr);
+		vkDestroyDevice(mDevice, nullptr);
+
+		// VkPhysicalDevice can’t be destroyed, as it’s not a Vulkan resource per-se, it’s more like just a handle to a GPU in the system.
+
+		vkb::destroy_debug_utils_messenger(mInstance, mDebugMessenger);
+		vkDestroyInstance(mInstance, nullptr);
+		
+		SDL_DestroyWindow(mWindow);
     }
 
     // clear engine pointer
     loadedEngine = nullptr;
 }
 
-void VulkanEngine::draw()
+void VulkanEngine::Draw()
 {
     // nothing yet
 }
 
-void VulkanEngine::run()
+void VulkanEngine::Run()
 {
     SDL_Event e;
     bool bQuit = false;
@@ -67,21 +214,21 @@ void VulkanEngine::run()
 
             if (e.type == SDL_WINDOWEVENT) {
                 if (e.window.event == SDL_WINDOWEVENT_MINIMIZED) {
-                    stop_rendering = true;
+                    mStopRendering = true;
                 }
                 if (e.window.event == SDL_WINDOWEVENT_RESTORED) {
-                    stop_rendering = false;
+                    mStopRendering = false;
                 }
             }
         }
 
         // do not draw if we are minimized
-        if (stop_rendering) {
+        if (mStopRendering) {
             // throttle the speed to avoid the endless spinning
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             continue;
         }
 
-        draw();
+        Draw();
     }
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,27 +5,70 @@
 
 #include <vk_types.h>
 
+struct FrameData {
+
+	VkCommandPool mCommandPool;
+	VkCommandBuffer mMainCommandBuffer;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
+
 class VulkanEngine {
 public:
 
-	bool _isInitialized{ false };
-	int _frameNumber {0};
-	bool stop_rendering{ false };
-	VkExtent2D _windowExtent{ 1700 , 900 };
+	bool mIsInitialized{ false };
+	int mFrameNumber {0};
+	bool mStopRendering{ false };
+	VkExtent2D mWindowExtent{ 1700 , 900 };
 
-	struct SDL_Window* _window{ nullptr };
+	struct SDL_Window* mWindow{ nullptr };
 
 	static VulkanEngine& Get();
 
 	//initializes everything in the engine
-	void init();
+	void Init();
 
 	//shuts down the engine
-	void cleanup();
+	void Cleanup();
 
 	//draw loop
-	void draw();
+	void Draw();
 
 	//run main loop
-	void run();
+	void Run();
+
+	// Vulkan Members
+	VkInstance mInstance; // Vulkan library handle
+	VkDebugUtilsMessengerEXT mDebugMessenger; // Vulkan debug output handle
+	VkPhysicalDevice mChosenGPU; // GPU chosen as the default device
+	VkDevice  mDevice; // Vulkan device for commands
+	VkSurfaceKHR mSurface; // Vulkan window surface
+
+	// Vulkan Swapchain Members
+	VkSwapchainKHR mSwapchain;
+	VkFormat mSwapchainImageFormat;
+
+	std::vector<VkImage> mSwapchainImages;
+	std::vector<VkImageView> mSwapchainImageViews;
+	VkExtent2D mSwapchainExtent;
+
+	// FrameData for holding our command pools
+	FrameData mFrames[FRAME_OVERLAP];
+	FrameData& GetCurrentFrame() { return mFrames[mFrameNumber % FRAME_OVERLAP]; }
+
+	VkQueue mGraphicsQueue;
+	uint32_t mGraphicsQueueFamily;
+
+private:
+	void InitVulkan();
+	
+	void InitSwapchain();
+	void CreateSwapchain(uint32_t width, uint32_t height);
+	void DestroySwapchain();
+	
+	void InitCommands();
+	void InitSyncStructures();
+
+
 };
